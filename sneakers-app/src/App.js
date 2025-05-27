ea58312fa11fe34d389c072eb9268df66f075bf9
Anton Krivelo <krivelo2017@mail.ru>
2025-05-27 11:33:46 +0300

    Added an array of sneakers and the map method to the Card component

--- sneakers-app/src/App.js
+++ sneakers-app/src/App.js
@@ -6,8 +6,15 @@ import SearchBlock from './Components/SearchBlock/SearchBlock';
 import './index.scss';
 
 
-
 function App() {
+
+const sneakersList = [
+  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imgUrl: '/Img/Sneakers/1.jpg'},
+  {name: 'Мужские Кроссовки Nike Air Max 270', price: 12999, imgUrl: './Img/Sneakers/2.jpg'},
+  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499},
+  {name: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999},
+]
+
   return (
     <div className="wrapper">
         <div className="container">
@@ -17,7 +24,9 @@ function App() {
               <h1 className="title">Все кроссовки:</h1>
                 <SearchBlock />
               <div className="content-item">
-                <Card />
+                {
+                  sneakersList.map((item) => <Card imgUrl={item.imgUrl} item={item} />)
+                }
               </div>
           </div>
         </div>
--- sneakers-app/src/Components/Card/Card.jsx
+++ sneakers-app/src/Components/Card/Card.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import "./Card.scss";
 import add from "../../Icons/CardIcon/add-button.svg";
-import img from "../../Img/img1.svg";
 import heartUnliked from "../../Icons/CardIcon/heart-unliked.svg";
 
-const Card = () => {
+const Card = ({item, imgUrl}) => {
+
     return (
         <div className="card-item">
             <button className="card-item-favourite">
                 <img className="card-item-favourite-img" src={heartUnliked} alt="FavouriteIcon" />
             </button>
-            <img className="card-item-img" src={img} alt="kross" />
-            <p className="card-item-name">Мужские Кроссовки Nike Blazer Mid Suede</p>
+            <img className="card-item-img" src={imgUrl} alt="sneakers-img" />
+            <p className="card-item-name">{item.name}</p>
             <div className="card-item-info">
                 <div className="card-item-info-price">
                     <span className="card-item-info-price-title">Цена:</span>
-                    <p className="card-item-price">12 999 Rub.</p>
+                    <p className="card-item-price">{item.price} Rub.</p>
                 </div>
                 <button className="card-item-add-button">
                     <img className="card-item-add-button-img" src={add} alt="added to card" />
