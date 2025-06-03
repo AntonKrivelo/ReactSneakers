48de8c3848d22a28e76b577a2ec59a3e05b7e024
Anton Krivelo <krivelo2017@mail.ru>
2025-06-03 20:13:35 +0300

    Added display components when there are items in the cart or not

--- sneakers-app/src/Components/Drawer/Drawer.jsx
+++ sneakers-app/src/Components/Drawer/Drawer.jsx
@@ -3,6 +3,7 @@ import "./Drawer.scss";
 import CartItem from "../CartItem/CartItem";
 import removeCart from "../../Icons/CartIcons/button-cart.svg";
 import vector from "../../Icons/vector.svg";
+import cartEmptyIcon from "../../Icons/CartIcons/cart-empty.svg";
 
 const Drawer = ({openOnClickCart, closeOverlayCart, cartItems = [], removeCartItem}) => {
     return (
@@ -10,28 +11,37 @@ const Drawer = ({openOnClickCart, closeOverlayCart, cartItems = [], removeCartIt
             <div className="drawer-block">
                 <h2 className="drawer-block-title">Корзина</h2>
                     <img onClick={openOnClickCart} className="drawer-block-close-cart" src={removeCart} alt="iconRemoveCart" />
-                <div className="drawer-block-items">
+                
+                    {
+                        cartItems.length > 0 ? <div className="drawer-block-items">
                     {
                         cartItems.map((obj) => (
                             <CartItem removeCartItem={removeCartItem} obj={obj} />
                         ))
                     }
-                </div>
-                <div className="drawer-block-buy">
-                    <div className="drawer-block-buy-total">
-                        <p>Итого:</p>
-                        <span>21 498 Rub.</span>
-                    </div>
+                     <div className="drawer-block-buy">
+                        <div className="drawer-block-buy-total">
+                            <p>Итого:</p>
+                            <span>0 Rub.</span>
+                        </div>
                     <div className="drawer-block-buy-tax">
                         <p>Налог 5%:</p>
-                        <span>1074 Rub.</span>
+                        <span>0 Rub.</span>
                     </div>
                     <div>
                         <button className="drawer-block-buy-btn">Оформить заказ
                             <img src={vector} alt="vector-icon" />
                         </button>
                     </div>
-                </div>
+                    </div>
+                    </div> : <div className="cart-empty">
+                        <img src={cartEmptyIcon} alt="cart-empty-icon" />
+                        <h2>Корзина пуста</h2>
+                        <p>Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ</p>
+                    </div>
+                    }
+                
+               
             </div>
         </div>
     )
