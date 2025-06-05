54b484c9722eadbf0c76b194d7c5606cd6645e2b
Anton Krivelo <krivelo2017@mail.ru>
2025-06-05 20:02:26 +0300

    I passed all the functions and statuses through the props

--- sneakers-app/src/App.js
+++ sneakers-app/src/App.js
@@ -1,26 +1,81 @@
-import './App.css';
-import React from 'react';
-import './index.scss';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 import Favourite from './Components/Favourite/Favourite';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
+import './index.scss';
 
 
 function App() {
 
+  
+const [items, setItems] = useState([]);
+const [searchValue, setSearchValue] = useState('');
+
+const [cartItems, setCartItems] = useState([]);
+const [favouriteItems, setFavouriteItems] = useState([]);
+
+const [openCart, setOpenCart] = useState(false);
+
+const openOnClickCart = () => {
+  setOpenCart(!openCart);
+}
+
+const closeOverlayCart = (e) => {
+  if(e.target.classList.contains('drawer-overlay')) {
+    setOpenCart(false)
+  }
+}
+
+const removeCartItem = (id) => {
+  axios.delete(`https://68385e662c55e01d184d08ef.mockapi.io/cart/${id}`)
+  setCartItems((prev) => prev.filter(item => item.id !== id))
+}
+
+useEffect(() => {
+      axios.get('https://68385e662c55e01d184d08ef.mockapi.io/items').then(res => {
+        setItems(res.data)
+      });
+      axios.get('https://68385e662c55e01d184d08ef.mockapi.io/cart').then(res => {
+        setCartItems(res.data)
+      })
+}, [])
 
 
+const onAddToCart = (obj) => {
+  axios.post('https://68385e662c55e01d184d08ef.mockapi.io/cart', obj)
+  setCartItems([...cartItems, obj])
+}
 
+const onAddToFavourite = (obj) => {
+  setFavouriteItems([...favouriteItems, obj])
+}
+
+const onChangeValue = (e) => {
+  setSearchValue(e.target.value)
+}
 
   return (
       <div className="wrapper">
-          <Home />
+         <Routes>
+            <Route path="/" element={<Home 
+            items={items} 
+            searchValue={searchValue} 
+            setSearchValue={setSearchValue} 
+            openOnClickCart={openOnClickCart}
+            removeCartItem={removeCartItem}
+            openCart={openCart}
+            cartItems={cartItems}
+            onAddToFavourite={onAddToFavourite}
+            onAddToCart={onAddToCart}
+            onChangeValue={onChangeValue}
+            closeOverlayCart={closeOverlayCart}
+            />} exact/>
+            <Route path="/favourite" element={<Favourite />} exact/>
+         </Routes>
       </div>
   );
 }
 
-                {/* <Routes>
-                  <Route path="/favourite" element={<Favourite/>} exact/>
-                </Routes> */}
 
 export default App;
--- sneakers-app/src/Components/Home/Home.jsx
+++ sneakers-app/src/Components/Home/Home.jsx
@@ -1,5 +1,4 @@
-import React,{useState, useEffect} from "react";
-import axios from 'axios';
+import React from "react";
 import SearchBlock from '../SearchBlock/SearchBlock';
 import Card from '../Card/Card';
 import Drawer from '../Drawer/Drawer';
@@ -8,80 +7,40 @@ import '../../index.scss';
 
 
 
-const Home = () => {
-
-
-const [items, setItems] = useState([]);
-const [searchValue, setSearchValue] = useState('');
-
-const [cartItems, setCartItems] = useState([]);
-const [favouriteItems, setFavouriteItems] = useState([]);
-
-
-const [openCart, setOpenCart] = useState(false);
-
-const openOnClickCart = () => {
-  setOpenCart(!openCart);
-}
-
-const closeOverlayCart = (e) => {
-  if(e.target.classList.contains('drawer-overlay')) {
-    setOpenCart(false)
-  }
-}
-
-
-const removeCartItem = (id) => {
-  axios.delete(`https://68385e662c55e01d184d08ef.mockapi.io/cart/${id}`)
-  setCartItems((prev) => prev.filter(item => item.id !== id))
-}
-
-useEffect(() => {
-      axios.get('https://68385e662c55e01d184d08ef.mockapi.io/items').then(res => {
-        setItems(res.data)
-      });
-      axios.get('https://68385e662c55e01d184d08ef.mockapi.io/cart').then(res => {
-        setCartItems(res.data)
-      })
-}, [])
-
-
-const onAddToCart = (obj) => {
-  axios.post('https://68385e662c55e01d184d08ef.mockapi.io/cart', obj)
-  setCartItems([...cartItems, obj])
-}
-
-const onAddToFavourite = (obj) => {
-  setFavouriteItems([...favouriteItems, obj])
-}
-
-const onChangeValue = (e) => {
-  setSearchValue(e.target.value)
-}
+const Home = ({items, 
+               searchValue, 
+               setSearchValue, 
+               openOnClickCart,
+               removeCartItem,
+               openCart,
+               cartItems,
+               onAddToFavourite,
+               onAddToCart,
+               onChangeValue,
+               closeOverlayCart
+              }) => {
 
 
     return (
-        <div className="home">
-            <div className="container">
-                <Header openOnClickCart={openOnClickCart} />
+        <div className="container">
+              <Header openOnClickCart={openOnClickCart} />
+              {
+              openCart ? <Drawer removeCartItem={removeCartItem} openOnClickCart={openOnClickCart} cartItems={cartItems} closeOverlayCart={closeOverlayCart} /> : null
+              }
+              <div className="container content"> 
+              <h1 className="title">{searchValue ? `Поиск по запросу: "${searchValue}"` : "Все кроссовки:"}</h1>
+                <SearchBlock value={searchValue} clearValue={() => setSearchValue('')} searchValue={searchValue} onChangeValue={onChangeValue} />
+              <div className="content-item">
                 {
-                openCart ? <Drawer removeCartItem={removeCartItem} openOnClickCart={openOnClickCart} cartItems={cartItems} closeOverlayCart={closeOverlayCart} /> : null
+                  items
+                  .filter((item) => item.name.toLowerCase().includes(searchValue.toLowerCase()))
+                  .map((item,index) => 
+                  <Card key={index} imgUrl={item.imgUrl} name={item.name} price={item.price} 
+                  onAddToCart={(obj) => onAddToCart(obj)} 
+                  onAddToFavourite={(obj) => onAddToFavourite(obj)} 
+                  />)
                 }
-                <div className="container content"> 
-                <h1 className="title">{searchValue ? `Поиск по запросу: "${searchValue}"` : "Все кроссовки:"}</h1>
-                  <SearchBlock value={searchValue} clearValue={() => setSearchValue('')} searchValue={searchValue} onChangeValue={onChangeValue} />
-                <div className="content-item">
-                  {
-                    items
-                    .filter((item) => item.name.toLowerCase().includes(searchValue.toLowerCase()))
-                    .map((item,index) => 
-                    <Card key={index} imgUrl={item.imgUrl} name={item.name} price={item.price} 
-                    onAddToCart={(obj) => onAddToCart(obj)} 
-                    onAddToFavourite={(obj) => onAddToFavourite(obj)} 
-                    />)
-                  }
-                </div>
-            </div>
+              </div>
             </div>
         </div>
     )
